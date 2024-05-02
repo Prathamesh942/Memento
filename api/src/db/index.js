da354608900812e5ef79c2499a5b6ba2c13132bf
prathamesh <prathameshsk94@gmail.com>
2024-05-02 14:39:23 +0530

    database connection

---
+++ api/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const instance = await mongoose.connect(`${process.env.MONGODB_URI}/Memento`);
+        console.log("Connected to mongodb", instance.connection.host);
+    } catch (error) {
+        console.log("Error connecting to mongodb");
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,11 @@
 import { app } from "./app.js"
+import connectDB from "./db/index.js"
 
-app.listen(process.env.PORT || 3000, ()=>{ console.log("server running on port 3000")})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{ console.log("server running on port 3000")});
+})
+.catch((err)=>{
+    console.log("Error, mongodb connection failed", err);
+})
+
