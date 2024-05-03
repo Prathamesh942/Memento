143157574f7e7047ed9389897b0e0057fd4b2a23
prathamesh <prathameshsk94@gmail.com>
2024-05-03 18:33:07 +0530

    add auth routes and controller

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,10 @@
 import express from "express"
 import userRouter from "./routes/user.routes.js"
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use("/api/v1/users", userRouter);
 
--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -7,5 +10,28 @@ const userSchema = new Schema({
     password: { type: String, required: true },
     profilePicture: { type: String } 
 });
+
+userSchema.pre("save", async function (next){
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+  };
 
 export const User = mongoose.model('User', userSchema);
